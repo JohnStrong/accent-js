1e8cf8db8830566b1c7ac0253fa90c183b3bbe01
John Strong <strngj411@gmail.com>
2014-07-04 13:32:26 +0100

    simplifed api with HOFs and functional composition

--- accent.js
+++ accent.js
@@ -17,14 +17,22 @@
 		HTMLCollection.prototype.forEach = 
 			Array.prototype.forEach;
 
+	//+ _compose :: array(function) -> function -> any
+	var _compose = function() {
+		var fns = Array.prototype.slice.call(arguments);
+
+		return function(arg) {
+			return fns.reduce(function(arg, fn) {
+				return fn(arg);
+			}, arg);
+		};
+	},
+
 	/**
-	 *	supported languages namespace
-	 *
+	 *	supported languages namespaces
 	 *	returns expressions and syntax wrappers for each specified language feature
-	 * 	
-	 *	e.g. language = { feature[String] : [ expression[Regexp], wrapper[String] ], ... }
 	 **/
-	var _language = {};
+	_language = {};
 
 	// helper methods for parsers of language features
 	_language['util'] = {
@@ -50,177 +58,133 @@
 	};
 
 	// the javascript language parser
-	_language['javascript'] = {
+	_language['javascript'] = [
 
 		// common language operators such as conditionals and loops
-		'operation': [
+		[
 			/\b(if|else|continue|switch|case|default|break|return|for|try|catch|throw)(?=[^\w])/g, 
 			_language.util.highlight('js-operation')
 		],
 
 		// variable assignment keywords
-		'declaration': [
+		[
 			/(\bfunction|var|const|in|new|this|prototype)(?=[^\w])/g, 
 			_language.util.highlight('js-declaration')
 		],
 
 		// frequently used methods
-		'specials': [
+		[
 			/(\.\bgetElementById|getElementsBy(ClassName|TagName|Name)|(type|instance)of|hasOwnProperty)/g, 
 			_language.util.highlight('js-special')
 		],
 
 		// common dom methods
-		'dom methods': [
+		[
 			/(\.\binnerHTML|createElement|parentNode|innerHTML|(append|replace)Child)(?=[^\w])/g,
 			_language.util.highlight('js-dom')
 		],
 
 		// globals [window]
-		'global': [
+		[
 			/\b(window|console|document)/g,
 			_language.util.highlight('js-global')
 		],
 
 		// basic types and special type checking keywords
-		'types': [
+		[
 			/\b(Array|String|Function|Object|Number|Date|Boolean|Error|RegExp|Math|null|undefined|true|false)(?=[^\w])/g, 
 			_language.util.highlight('js-type')
 		],
 
 		// numeric values (including hexadecimal)
-		'number': [
+		[
 			/(-{0,1}\d+\.{0,1}\d+|-{0,1}0x\w+)(?=[^\w])/g, 
 			_language.util.highlight('js-numeric')
 		],
 
 		// double/single qouted string
-		'string': [
+		[
 			/(".*?"|'.*?')/g, 
 			_language.util.ignoreNot('js-string')
 		],
 
 		// comments
-		'comment': [
+		[
 			/(\/{2}.*?\n+|\/\*(.|[\r\n])*\*\/)/g,
 			_language.util.ignoreNot('js-comment')
 		],
 
 		// regexp literals
-		'regExp': [
+		[
 			/[^<](\/.+?\/(\s|,|\]|;|\/|g|i|m|y))/g,
 			_language.util.ignoreNot('js-regexp')
 		],
-	};
-
-
-	// primary syntax highlighter namespace
-	var _accent = {};
+	];
 
 	// formats the code content with a default theme
-	_accent['format'] = ( function() {
-		
-		// formats dom node by wrapping it with a HTML pre node
-		var _wrapWith = function(domNode, wrapper) {
-			var parent = domNode.parentNode,
-				pre = document.createElement(wrapper);
-
-				parent.replaceChild(pre, domNode);
-				pre.appendChild(domNode);
-		};
-
-		return function(domNode) {
-
+	var _format = function(theme) {
+		return function(node) {
 			// wrap with pre formatter tag
-			_wrapWith(domNode, 'pre');
+			var parent = node.parentNode,
+			pre = document.createElement('pre');
+
+			parent.replaceChild(pre, node);
+			pre.appendChild(node);
 
 			// prefix theme with the acc flag
-			domNode.parentNode.className = 'acc-'.concat(this.theme);
-		};
+			node.parentNode.className = 'acc-'.concat(theme);
 
-	} )();
+			return node;
+		};
+	},
 
 	// parses element text body according to the selected language property rules (see language)
-	_accent['parse'] = ( function() {
-
-		var _byFeature = function(textBody, langFeature) {
-			return textBody.replace(langFeature[0], langFeature[1]);
-		};
-		
-		// return highlighted text of given dom node for given language
-		return function(domNode) {
-			
-			var textBody = domNode.innerHTML,
-				lang = this.lang;
-
-			
-			// apply language highlighting rules to text
-			for(var feature in lang) {
-				if(lang.hasOwnProperty(feature)) {	
-					textBody = _byFeature(textBody, lang[feature]);
-				}
-			}
-			
-			// replace plain node text body with highlighted text body
-			domNode.innerHTML = textBody;
-		};
-
-	} )();
-
-	// runner mathod for accent sytax highlighting
-	_accent['highlight'] = function(config) {
-		
-		var self = this;
-
+	_parse = function(lang) {
 		return function(node) {
-			self.format.call(config, node);
-			self.parse.call(config, node);	
-		};
-	};
+			node.innerHTML = lang.reduce(function(textBody, grammer) {
+				return textBody.replace(grammer[0], grammer[1]);
+			}, node.innerHTML);
 
+			return node;
+		};
+	},
 
 	// outward facing reference
 	// clean up closure functionality, implment a less general condition evaluation function
-	var accent = ( function(accent, language) {
+	accent = (function() {
 
-		var _errors = {
-			illegalArgumentsError: 'both identifier and language parameters must be strings'
-		},
-		
 		// reliable way to check type of accent function arguments
-		_is = function(obj, type) {
+		var _is = function(obj, type) {
 			var clas = Object.prototype.toString.call(obj).slice(8, -1);
 			return clas === type;
+		},
+
+		_highlight = function(lang, theme) {
+			return function(elem) {
+				_compose(_format(theme), _parse(lang))(elem);
+			};
 		};
 
 		// add option param
 		return function(identifier, lang, theme) {
 
-			var config = {},
-				elems, 
-				acc,
-
 			// basic input validation of function params
-			isidentifierString = _is(identifier, 'String'),
+			var isidentifierString = _is(identifier, 'String'),
 			isLangString = _is(lang, 'String'),
 			isThemeString = _is(theme, 'String');
 			
 			if(isidentifierString && isLangString && isThemeString) {
 				
-				elems = document.getElementsByClassName(identifier);
-
-				config.lang = language[lang];
-				config.theme = theme;
+				var elems = document.getElementsByClassName(identifier),
+				begin = _highlight(_language[lang], theme);
+				elems.forEach(begin);
 
 			} else {
-				throw new Error(_errors.illegalArgumentsError);
+				throw new TypeError('both identifier and language parameters must be strings');
 			}
-
-			acc = accent['highlight'](config);
-			elems.forEach(acc);
 		};
 
-	})(_accent, _language);
+	})();
 
 	/**
 	 *	usage => accent(_className, _language, _theme)
